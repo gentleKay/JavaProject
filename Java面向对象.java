ebc761ef61fe39d60f2578db78dadc96a931f254
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-07-30 21:28:47 +0800

    Update Java面向对象.java

--- Java面向对象.java
+++ Java面向对象.java
@@ -48,3 +48,51 @@ public class Student{
 		s.study();
 	}
 }
+//一个类作为 形式参数 变量 进行的调用。  
+package hello;
+
+class S {
+	public void study() {
+		System.out.println("love study!");
+	}
+}
+class Stu{
+	public void method(S s) {
+		s.study();
+	}
+}
+public class Student{
+	public static void main(String[] args) {
+		Stu stu = new Stu();
+		S s = new S();
+		stu.method(s);
+	}
+}
+//匿名对象。 匿名对象调用完毕后就是垃圾，可以被垃圾回收器回收。
+package hello;
+
+class S {
+	public void study() {
+		System.out.println("love study!");
+	}
+}
+class Stu{
+	public void method(S s) {  // 这里的形式参数是 （引用类型）对象。
+		s.study();
+	}
+}
+public class Student{
+	public static void main(String[] args) {
+		S s = new S();
+		s.study();
+		s.study();   //这里两次的输出是同一个对此昂进行的输出。
+		System.out.println("--------------");
+		new S().study();
+		new S().study();    //而这里的两次输出却是不同的两个对象进行的输出。
+		System.out.println("--------------");
+		Stu stu = new Stu();
+		stu.method(s);
+		System.out.println("--------------");
+		new Stu().method(new S());    // 这里用到的是 匿名对象的方法。
+	}
+}
