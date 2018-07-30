8b423e1500700a968659bed323a4c99df4d0b53d
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-07-30 19:31:09 +0800

    Create Java面向对象.java

---
+++ Java面向对象.java
@@ -0,0 +1,17 @@
+//定义一个简单的学生类。
+package hello;
+
+public class Student {
+	String name;
+	int age;
+	String address;
+	
+	public void study() {
+		System.out.println("love study!");
+	}
+	
+	public void eat() {
+		System.out.println("eat dinner!");
+	}
+}
+//
