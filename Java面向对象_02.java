1247702e05156b8c0c488f7eaffa2ba9d8363411
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-07-30 22:15:24 +0800

    Update Java面向对象_02.java

--- Java面向对象_02.java
+++ Java面向对象_02.java
@@ -26,3 +26,33 @@ public class Student{
 		System.out.println(stu.getName()+" "+stu.getAge());
 	}
 }
+//this:是当前类的对象引用，简单的记，它就代表当前类的一个对象。
+//注意： 谁调用了这个方法，在该方法内部的 this 就代表谁。
+//this的场景：解决局部变量  隐藏成员变量。
+package hello;
+
+class Stu{
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+}
+public class Student{
+	public static void main(String[] args) {
+		Stu stu = new Stu();
+		stu.setName("GSK");
+		stu.setAge(22);
+		System.out.println(stu.getName()+" "+stu.getAge());
+	}
+}
