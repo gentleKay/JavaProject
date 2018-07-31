b858ca6bb07efaa7c1fdba1bbb8c27c4682032bc
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-07-31 14:57:19 +0800

    Update Java面向对象_02.java

--- Java面向对象_02.java
+++ Java面向对象_02.java
@@ -56,3 +56,63 @@ public class Student{
 		System.out.println(stu.getName()+" "+stu.getAge());
 	}
 }
+// 私有变量，必须要用函数 且用 参数 进行调用。
+package hello;
+import java.util.Scanner;
+class Dome{
+	private int Long;
+	private int Wide;
+	
+	public int Perimeter(int Long,int Wide) {
+		return 2*(Long+Wide);
+	}
+	public int Area(int Long,int Wide) {
+		return Long*Wide;
+	}
+}
+public class Test{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入长：");
+		int L = sc.nextInt();
+		System.out.print("请输入宽：");
+		int W = sc.nextInt();
+		Dome d = new Dome();
+		//d.Long = L;
+		//d.Wide = W;
+		int P = d.Perimeter(L,W);
+		int S = d.Area(L,W);
+		System.out.println("周长："+ P);
+		System.out.println("面积："+ S);
+	}
+}
+//键盘输入长宽，计算它的周长和面积，要用面向对象的方法。
+package hello;
+import java.util.Scanner;
+class Dome{
+	int Long;
+	int Wide;
+	
+	public int Perimeter() {
+		return 2*(Long+Wide);
+	}
+	public int Area() {
+		return Long*Wide;
+	}
+}
+public class Student{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入长：");
+		int L = sc.nextInt();
+		System.out.print("请输入宽：");
+		int W = sc.nextInt();
+		Dome d = new Dome();
+		d.Long = L;
+		d.Wide = W;
+		int P = d.Perimeter();
+		int S = d.Area();
+		System.out.println("周长："+ P);
+		System.out.println("面积："+ S);
+	}
+}
