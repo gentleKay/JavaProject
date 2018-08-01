0240f0ac4597a7e7cc124899797694bf544187f7
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-08-01 13:30:35 +0800

    Create Java面向对象_继承.java

---
+++ Java面向对象_继承.java
@@ -0,0 +1,13 @@
+// Java中的继承只能是 单继承 不能是多继承。
+package hello;
+//import java.util.Scanner;
+class Father{}
+class Mothrt{}
+class Son extends Father{}
+//class Son extends Mother{}  //因为Son 已经继承了Father类了，所以Son类将不能再继承Mother类。
+public class Student{
+	public static void main(String[] args) {
+		Son s = new son;
+	}
+}
+//
