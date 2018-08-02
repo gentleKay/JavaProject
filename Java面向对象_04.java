ce09a0d24e0494872a499b1afba135403fc5268c
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-08-02 16:13:53 +0800

    Update Java面向对象_04.java

--- Java面向对象_04.java
+++ Java面向对象_04.java
@@ -19,3 +19,56 @@ public class Student{
 		z.show();
 	}
 }
+// 多态。 猫，狗，动物类的案例演示。
+package hello;
+
+class Animal{
+	public Animal() {}
+	public void eat() {
+		System.out.println("吃饭！");
+	}
+}
+
+class Cat extends Animal{
+	public Cat() {}
+	public void eat() {
+		System.out.println("吃鱼！");
+	}
+	public void play() {
+		System.out.println("猫玩！");
+	}
+}
+
+class Dog extends Animal{
+	public Dog() {}
+	public void eat() {
+		System.out.println("吃肉！");
+	}
+	public void look() {
+		System.out.println("狗看门!");
+	}
+}
+
+public class Student{
+	public static void main(String[] args) {
+		Animal a = new Dog();
+		a.eat();
+		//a.look();  这个是错误的，子类可以用父类的函数，但是父类不能访问使用子类的函数
+		System.out.println("----------");
+		Dog d = (Dog)a;
+		d.eat();
+		d.look();
+		System.out.println("----------");
+		a = new Cat();
+		a.eat();
+		System.out.println("----------");
+		Cat c =(Cat)a;
+		c.eat();
+		c.play();
+		System.out.println("----------");
+		//Dog dd = (Dog)a; 
+		// 这个 对象a 是 猫类的，并不是动物类，所以这句语法有错误。
+	}
+}
+//
+
