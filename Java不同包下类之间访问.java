159d2a2b3b69ba6b0375ea87f8a3b6894c69855b
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-08-07 12:27:42 +0800

    Update Java不同包下类之间访问.java

--- Java不同包下类之间访问.java
+++ Java不同包下类之间访问.java
@@ -8,11 +8,14 @@ public class Test {
 }
 //这个是hello包  //程序是在有主程序的地方调试编译的
 package hello;
-
+import demo.Test;  // 这里可以用到关键字 import； 导入包 demo.Test.  //格式：包.类名
 public class world {
 	public static void main(String[] args) {
-		demo.Test d = new demo.Test();
-		System.out.println(d.sum(10, 20));
+		//demo.Test t = new demo.Test();
+		//System.out.println(t.sum(10, 20));
+		System.out.println("-------------");
+		Test t = new Test();
+		System.out.println(t.sum(10, 20));
 	}
 }
 //
