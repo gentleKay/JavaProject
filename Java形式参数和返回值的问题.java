672f9d74a8972894fa0a764ec22cedcad0a62609
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-08-07 20:40:36 +0800

    Update Java形式参数和返回值的问题.java

--- Java形式参数和返回值的问题.java
+++ Java形式参数和返回值的问题.java
@@ -1,1 +1,25 @@
 //形式参数
+//类名作为形式参数
+class Student {
+  public void study(){
+    System.out.println("Study!");
+  }
+}
+
+class StudentDemo{
+  public void method(Student s){  ss = new Student();    Student s = new Student();
+    s.study();
+  }
+}
+class StudentTest{
+  public static void main(String[] args){
+    Student s =  new Student();
+    s.study();
+    System.out.println("----------------");
+    StudentDemo sd = new StudentDemo();
+    Student ss = new Student();
+    sd.method(ss);
+    System.out.println("----------------");
+    new StudentDemo().method (new Student());  // 匿名对象用法
+  }
+}
