a5a635f2d4310dbe213a6e74c19242ba3fd463d5
gentle_Kay <37505624+gentleKay@users.noreply.github.com>
2018-08-07 20:59:13 +0800

    Update Java形式参数和返回值的问题.java

--- Java形式参数和返回值的问题.java
+++ Java形式参数和返回值的问题.java
@@ -23,3 +23,55 @@ class StudentTest{
     new StudentDemo().method (new Student());  // 匿名对象用法
   }
 }
+//
+package hello;
+
+abstract class Person{
+	public abstract void study();
+}
+
+class PersonDemo{
+	public void method(Person p) {
+		p.study();
+	}
+}
+
+class Stu extends Person{
+	public void study() {
+		System.out.println("study!");
+	}
+}
+
+class Student{
+	public static void main(String[] args) {
+		PersonDemo pd = new PersonDemo();
+		Person p = new Stu();
+		pd.method(p);
+	} 
+}
+//
+package hello;
+
+interface Love{
+	public abstract void love();
+} 
+
+class LoveDemo{
+	public void method(Love l) {
+		l.love();
+	}
+}
+
+class LoveTest implements Love{
+	public void love() {
+		System.out.println("Love!");
+	}
+}
+
+class Student{
+	public static void main(String[] args) {
+		LoveDemo ld = new LoveDemo();
+		Love l = new LoveTest();
+		ld.method(l);
+	} 
+}
